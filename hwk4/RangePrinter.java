09e8e2e2121dad175a010d6cae59251313e12d28
Alpay Sinan Çolakoğlu <baoalpay@gmail.com>
2025-12-28 16:05:58 +0300

    Satisfying requirement change two

--- hwk4/RangePrinter.java
+++ hwk4/RangePrinter.java
@@ -16,12 +16,15 @@ public class RangePrinter {
     }
     
     public static void printRange(int start, int end) {
-        if (start < end) {
-            for (int i = start + 1; i < end; i++) {
+    if (start < end) {
+        for (int i = start + 1; i < end; i++) {
+            if (i % 2 != 0) {  // odd numbers only
                 System.out.println(i);
             }
-        } else {
-            for (int i = start - 1; i > end; i--) {
+        }
+    } else {
+        for (int i = start - 1; i > end; i--) {
+            if (i % 2 != 0) {  // odd numbers only
                 System.out.println(i);
             }
         }
